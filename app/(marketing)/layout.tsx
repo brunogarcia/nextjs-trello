4825daecb2d714529e3dd1241fc2dd8ca67874ea
Bruno <bruno.garcia.echegaray@gmail.com>
2023-11-27 19:27:16 +0100

    feat: add new font and dependencies, create marketing layout and page, and add UI button component

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-full bg-slate-100">
+      {/* Navbar */}
+      <main className="pt-40 pb-20 bg-slate-100">{children}</main>
+    </div>
+  );
+}
