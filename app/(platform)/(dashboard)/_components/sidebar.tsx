aea0e7c47cc2a79835706ed0a9cf0a376b6d7814
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-01 09:56:48 +0100

    feat: add layouts for organization

---
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,3 @@
+export const Sidebar = () => {
+  return <div className="bg-red-50">sidebar</div>;
+};
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,7 @@
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
-      <div className="px-2 md:px-4">Hi!</div>
+      <div className="px-2 md:px-4">The organization</div>
     </div>
   );
 };
