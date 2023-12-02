13693398552bb90022599eb4d3a61f144b8bcf51
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-02 17:47:58 +0100

    feat(organization) add board list

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,18 @@
+import { Suspense } from "react";
+
+import { Separator } from "@/components/ui/separator";
+
+import { BoardList } from "./_components/board-list";
+
 const OrganizationIdPage = async () => {
   return (
     <div className="w-full mb-20">
-      <div className="px-2 md:px-4">The organization</div>
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
     </div>
   );
 };
