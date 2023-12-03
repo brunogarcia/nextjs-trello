f9f373f7d397949c26b40093cf1f2d4b7b023750
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-03 17:54:31 +0100

    feat(dashboard) add form popover

--- components/forms/form-popover.tsx
+++ components/forms/form-popover.tsx
@@ -18,6 +18,7 @@ import { useProModal } from "@/hooks/use-pro-modal";
 
 import { FormInput } from "./form-input";
 import { FormSubmit } from "./form-submit";
+import { FormPicker } from "./form-picker";
 
 interface FormPopoverProps {
   children: React.ReactNode;
@@ -77,6 +78,7 @@ export const FormPopover = ({
         </PopoverClose>
         <form action={onSubmit} className="space-y-4">
           <div className="space-y-4">
+            <FormPicker id="image" errors={fieldErrors} />
             <FormInput
               id="title"
               label="Board title"
