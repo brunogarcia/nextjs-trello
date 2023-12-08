9657ec2846edd815fa1cc28dbaf188d652eda2b1
Bruno <bruno.garcia.echegaray@gmail.com>
2023-12-08 17:41:42 +0100

    feat(api) retrieves the audit logs for a specific card

--- app/api/cards/[cardId]/logs/route.ts
+++ app/api/cards/[cardId]/logs/route.ts
@@ -4,6 +4,12 @@ import { ENTITY_TYPE } from "@prisma/client";
 
 import { db } from "@/lib/db";
 
+/**
+ * Retrieves the audit logs for a specific card.
+ * @param request - The HTTP request object.
+ * @param params - The parameters object containing the cardId.
+ * @returns A JSON response containing the audit logs.
+ */
 export async function GET(
   request: Request,
   { params }: { params: { cardId: string } }
